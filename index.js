48da34e4f27484d93f076ed7c75a1785ebcf5ceb
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-04-07 23:07:24 +0900

    detect whitespace sequence

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import { visit } from "unist-util-visit";
+import { is } from "unist-util-is";
+
 export default function retextSentenceSpacing() {
   return (tree, file) => {
     visit(tree, "ParagraphNode", (node) => {
-      console.log(node);
+      const children = node.children;
+
+      children.forEach((child, index) => {
+        if (
+          is(children[index - 1], "SentenceNode") &&
+          is(child, "WhiteSpaceNode") &&
+          is(children[index + 1], "SentenceNode")
+        ) {
+          console.log(child);
+        }
+      });
     });
   };
 }
