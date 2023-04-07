123c04701f93da35d4bd1cb44ee91a65a613bfeb
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-04-07 22:47:17 +0900

    working example

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-import { read } from "to-vfile";
-import { unified } from "unified";
-import remarkParse from "remark-parse";
-import remarkGfm from "remark-gfm";
-import remarkRehype from "remark-rehype";
-import rehypeStringify from "rehype-stringify";
-
-async function main() {
-  const file = await unified()
-    .use(remarkParse)
-    .use(remarkGfm)
-    .use(remarkRehype)
-    .use(rehypeStringify)
-    .process(await read("example.md"));
-  console.log(String(file));
+import { visit } from "unist-util-visit";
+export default function retextSentenceSpacing() {
+  return (tree, file) => {
+    visit(tree, "ParagraphNode", (node) => {
+      console.log(node);
+    });
+  };
 }
-
-main();
