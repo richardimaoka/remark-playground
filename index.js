6bbe44765653e71506daa0f62ddced108a962942
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-04-07 21:50:20 +0900

    at least working

---
+++ index.js
@@ -0,0 +1,18 @@
+import { read } from "to-vfile";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+
+async function main() {
+  const file = await unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypeStringify)
+    .process(await read("example.md"));
+  console.log(String(file));
+}
+
+main();
