83fafdc26a6df5107ed454ca2a942418ebe0c344
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-05-19 21:06:42 +0900

    rehyp react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { createElement, Fragment, useEffect, useState } from "react";
+import rehypeParse from "rehype-parse";
+import rehypeReact from "rehype-react";
+import { unified } from "unified";
+
+function useProcessor(text: string) {
+  const [Content, setContent] = useState(<></>);
+
+  useEffect(() => {
+    unified()
+      .use(rehypeParse, { fragment: true })
+      .use(rehypeReact, { createElement, Fragment })
+      .process(text)
+      .then((file) => {
+        setContent(file.result);
+      });
+  }, [text]);
+
+  return Content;
 }
 
-export default App;
+export default function App() {
+  const text = `<h2>Hello, world!</h2>
+<p>Welcome to my page 👀</p>`;
+
+  return useProcessor(text);
+}
